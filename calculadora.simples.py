918b80033812c87433beb6da8e403fb848debddf
SirCaio55 <caioluizgsa68@gmail.com>
2025-12-28 18:42:18 -0300

    primeiro commit

---
+++ calculadora.simples.py
@@ -0,0 +1,25 @@
+print('CALCULADORA')
+print('+ Adição')
+print('- Subtração')
+print('* Multiplicação')
+print('/ Divisão')
+print('Pressione qualquer outra tecla para sair.')
+
+op = input('Qual operação deseja realizar?')
+x = int(input('Digite o primeiro valor: '))
+y = int(input('Digite o segundo valor: '))
+
+if (op == '+'):
+    res = x + y
+    print(f'Resultado: {x} + {y} = {res}')
+elif (op == '-'):
+    res = x - y
+    print(f'Resultado: {x} - {y} = {res}')
+elif (op == '*'):
+    res = x * y
+    print(f'Resultado: {x} * {y} = {res}')
+elif (op == '/'):
+    res = x / y
+    print(f'Resultado: {x} / {y} = {res}')
+else:
+    print('Encerrando o programa...')
